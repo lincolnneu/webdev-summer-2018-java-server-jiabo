0ba1f0725f9210c1ecadaa04f991fbc49d42da36
Jiabo Hu <lincolnhuj@gmail.com>
2018-05-19 14:20:07 -0700

    hw1: new spring starter project

---
+++ src/main/java/com/example/demo/WebdevSummer2018JavaServerJiaboApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdevSummer2018JavaServerJiaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdevSummer2018JavaServerJiaboApplication.class, args);
+	}
+}
