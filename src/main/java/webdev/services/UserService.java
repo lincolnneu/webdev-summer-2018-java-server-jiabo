d56255999df5dbfc81372f91ef209285f37d2d3e
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-22 12:11:04 -0700

    hw1: forgot password: backend: implemented forgotPassword, sendEmail, resetPassword and findUserByEmail. So far I canot generate secret random link to reset password, but we can send a static link. I may fix this if I learn more about security. forgotPassword method will firstly check if the email is within the records. If so, we will send the reset link to the email. Otherwise, return an error. resetPassword will also do the record check first.

--- src/main/java/webdev/services/UserService.java
+++ src/main/java/webdev/services/UserService.java
@@ -3,9 +3,15 @@ package webdev.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import webdev.models.User;
 import webdev.repositories.UserRepository;
@@ -25,7 +30,41 @@ public class UserService {
 	@Autowired // instead of us instantiating repo, we ask a framework to inject it into that variable
 	UserRepository repository;
 	
-	@DeleteMapping("/api/user/{userId}") //--->|
+    @Autowired
+    private JavaMailSender sender;
+
+    @PostMapping("/api/forgotPassword")
+    public ResponseEntity<?> forgotPassword(HttpServletRequest request, @RequestBody User user) {
+    	User record = findUserByEmail(user.getEmail());
+    	if( record != null) {
+    		try {
+    	    	String url = request.getRequestURL().toString();
+    	        sendEmail(url, record);
+    	        return new ResponseEntity<User>(HttpStatus.OK);
+    	    } catch(Exception ex) {
+    	    	return new ResponseEntity<User>(HttpStatus.CONFLICT);
+    	    }
+
+    	} else {
+//    		System.out.println("the email does not match any record");
+    		 return new ResponseEntity<User>(HttpStatus.CONFLICT);
+    	}
+    }    
+
+    private void sendEmail(String url, User record) throws Exception{
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+        
+        helper.setTo(record.getEmail());
+        helper.setText("Please go to this link to reset your password: " + url.substring(0, url.length() - 19) + "/jquery/components/password/resetPassword.template.client.html");
+        helper.setSubject("Reset your password");
+        
+        sender.send(message);
+
+    }
+	
+	
+	@DeleteMapping("/api/user/{userId}") 
 	public void deleteUser(@PathVariable("userId") int id) { // parse the userId to an int
 		repository.deleteById(id);
 	}
@@ -40,12 +79,12 @@ public class UserService {
 	@PostMapping("/api/register")
 	public User register(@RequestBody User user, HttpSession session) {
 		if(findUserByUserName(user.getUsername()) == null) {
-			System.out.println("We don't have this entry. Accept");
+//			System.out.println("We don't have this entry. Accept");
 			User newUser =  createUser(user);
 			session.setAttribute("currentUser", newUser);
 			return newUser;
 		} else {
-			System.out.println("Yes we have. Reject");
+//			System.out.println("Yes we have. Reject");
 			return null;
 		}
 	}
@@ -54,7 +93,6 @@ public class UserService {
 	public User profile(HttpSession session) {
 		User currentUser = (User)
 		session.getAttribute("currentUser");
-//		System.out.println(currentUser.getUsername());
 		return currentUser;
 	}
 
@@ -62,7 +100,6 @@ public class UserService {
 	public void logout
 	(HttpSession session) {
 		session.invalidate();
-		System.out.println("=============== log out success=================");
 	}
 	
 	@PostMapping("/api/login")
@@ -71,13 +108,8 @@ public class UserService {
 		if(data.isPresent()) {
 			User foundUser = data.get();
 			user.setAttribute("currentUser", foundUser);
-			System.out.println("===============72 login=================");
-			System.out.println(foundUser.getUsername());
-			System.out.println(user.getAttribute("currentUser"));
-			System.out.println("===============74 login=================");
 			return foundUser;
 		} else {
-			System.out.println("===============77 login failed=================");
 			return null;
 		}
 		 
@@ -96,7 +128,6 @@ public class UserService {
 		Optional<User> data = repository.findById(userId); // findById could return null, so we should declare it as Optional from util
 		if(data.isPresent()) {
 			User user = data.get();
-			user.setUsername(newUser.getUsername());
 			user.setFirstName(newUser.getFirstName()); // local object
 			user.setLastName(newUser.getLastName());
 			user.setPassword(newUser.getPassword());
@@ -123,6 +154,20 @@ public class UserService {
 		return null;
 	}
 	
+	// a put mapping
+	@PutMapping("/api/resetPassword")
+	public ResponseEntity<?> resetPassword(@RequestBody User newUser) { // RequestBody map it to a user object newUser
+		User record = findUserByUserName(newUser.getUsername());
+		if(record != null) {
+			record.setPassword(newUser.getPassword());
+			repository.save(record);
+			return new ResponseEntity<User>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<User>(HttpStatus.CONFLICT); //appropriate error code
+		}
+	    
+	}
+	
 	@GetMapping("/api/user/{userId}")  // this is mapped to a get request
 	public User findUserById(@PathVariable("userId") int userId){ // parse the userId to an int
 		Optional<User> data = repository.findById(userId); // findById could return null, so we should declare it as Optional from util
@@ -134,11 +179,21 @@ public class UserService {
 	
 	@GetMapping("/api/register/{username}")  // this is mapped to a get request
 	public User findUserByUserName(@PathVariable("username") String username) {
-		System.out.println(username);
+//		System.out.println(username);
 		Optional<User> data = repository.findUserByUserName(username); // findUserByUserName could return null, so we should declare it as Optional from util
 		if(data.isPresent()) {
 			return data.get();
 		}
+		return null;
+	}
+	
+	@GetMapping("/api/email/{email}")  // this is mapped to a get request
+	public User findUserByEmail(@PathVariable("email") String email) {
+		Optional<User> data = repository.findUserByEmail(email); // findUserByUserName could return null, so we should declare it as Optional from util
+		if(data.isPresent()) {
+			return data.get();
+		}
+//		System.out.println("cannot find a user with the email, return null");
 		return null;
 	}
 	
