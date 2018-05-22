a6a2382c32a875c24dc811a0811f507c5c8d2290
lincolnneu <hu.jiab@husky.neu.edu>
2018-05-22 12:19:19 -0700

    hw1: forgot password: reset password: implemented controller for reseting password.

---
+++ src/main/webapp/jquery/components/password/resetPassword.controller.client.js
@@ -0,0 +1,44 @@
+(function () {
+    var $usernameFld, $newPasswordFld, $confirmNewPasswordFld;
+    var $resetBtn;
+    var userService = new UserServiceClient();
+    $(main);
+
+    function main() {
+        $usernameFld = $("#usernameFld");
+        $newPasswordFld = $("#newPasswordFld");
+        $confirmNewPasswordFld = $("#confirmNewPasswordFld");
+        $resetBtn = $("#resetBtn")
+            .click(resetPassword);
+
+    }
+    function resetPassword() {
+        if($newPasswordFld.val() != $confirmNewPasswordFld.val()){
+            alert("Two password are different. Please make sure they are the same.");
+        } else{
+            console.log($usernameFld.val());
+            console.log($newPasswordFld.val());
+
+            var user = new User($usernameFld.val(),$newPasswordFld.val(),null,null,null,null,null,null);
+
+            userService
+                .resetPassword(user)
+                .then(function(response){
+                    if(response.status == 200){
+                        success();
+                    } else{
+                        fail();
+                    }
+                });
+        }
+
+    }
+
+    function success(){
+        alert("Congratulations, the password has been reset successfully!");
+    }
+
+    function fail(){
+        alert("Sorry, the username does not exist.");
+    }
+})();
