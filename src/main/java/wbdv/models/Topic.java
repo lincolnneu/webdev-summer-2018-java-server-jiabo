5416b64eda6c0c98b978f741b212c4711ed227ba
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-02 22:29:24 -0700

    model: Added Topic model.

---
+++ src/main/java/wbdv/models/Topic.java
@@ -0,0 +1,38 @@
+package wbdv.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Topic {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	@ManyToOne
+	@JsonIgnore
+	private Lesson lesson;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Lesson getLesson() {
+		return lesson;
+	}
+	public void setLesson(Lesson lesson) {
+		this.lesson = lesson;
+	}
+}
