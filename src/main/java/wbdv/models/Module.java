4e45488f405ffd4b75876a8a7c9118aa84fffc4a
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-02 22:23:56 -0700

    module Model: refactor the module model to support the one to many relation between Modules and lessons.

--- src/main/java/wbdv/models/Module.java
+++ src/main/java/wbdv/models/Module.java
@@ -1,10 +1,13 @@
 package wbdv.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -17,6 +20,8 @@ public class Module {
 	@ManyToOne
 	@JsonIgnore // browser will get infinite json. This is a break of the infinite loop. 
 	private Course course;
+	@OneToMany(mappedBy="module")
+	private List<Lesson> lessons; //To support the one to many relation between Modules and lessons.
 	public int getId() {
 		return id;
 	}
@@ -35,4 +40,11 @@ public class Module {
 	public void setCourse(Course course) {
 		this.course = course;
 	}
+	public List<Lesson> getLessons() {
+		return lessons;
+	}
+	public void setLessons(List<Lesson> lessons) {
+		this.lessons = lessons;
+	}
+	
 }
