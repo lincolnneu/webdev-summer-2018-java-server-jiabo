18aae3ae2e2aa6f73af4d21d0e7304ce23865c1e
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-21 23:45:28 -0700

    Widget: Exam: added exam and its repo and service. It inherits from widget.

---
+++ src/main/java/wbdv/repositories/ExamRepository.java
@@ -0,0 +1,10 @@
+package wbdv.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import wbdv.models.Exam;
+
+public interface ExamRepository extends CrudRepository<Exam, Integer>{
+	
+	
+}
---
+++ src/main/java/wbdv/services/ExamService.java
@@ -0,0 +1,59 @@
+package wbdv.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import wbdv.models.Exam;
+import wbdv.models.MultipleChoiceQuestion;
+import wbdv.models.Question;
+import wbdv.models.TrueFalseQuestion;
+import wbdv.repositories.ExamRepository;
+import wbdv.repositories.TrueFalseQuestionRepository;
+import wbdv.repositories.MultipleChoiceQuestionRepository;
+
+@RestController
+@CrossOrigin(origins="*")
+public class ExamService {
+	@Autowired
+	ExamRepository examRepository;
+	@Autowired
+	TrueFalseQuestionRepository trueFalseRepository;
+	@Autowired
+	MultipleChoiceQuestionRepository multiRepository;
+	
+	@GetMapping("/api/multi/{questionId}")
+	public MultipleChoiceQuestion findMultiQuestionById(@PathVariable("questionId") int questionId) {
+		Optional<MultipleChoiceQuestion> optional = multiRepository.findById(questionId);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+	
+	@GetMapping("/api/truefalse/{questionId}")
+	public TrueFalseQuestion findTrueFalseQuestionById(@PathVariable("questionId") int questionId) {
+		Optional<TrueFalseQuestion> optional = trueFalseRepository.findById(questionId);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+	}
+	
+	@GetMapping("/api/exam/{examId}/question")
+	public List<Question> findAllQuestionsForExam(@PathVariable("examId") int examId) {
+		Optional<Exam> optionalExam = examRepository.findById(examId);
+		if(optionalExam.isPresent()) {
+			Exam exam = optionalExam.get();
+			List<Question> questions = exam.getQuestions();
+			int count = questions.size();
+			return questions;
+		}
+		return null;
+	}
+}
