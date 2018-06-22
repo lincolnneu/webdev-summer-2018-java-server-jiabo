225bb533f36318d6063bb0a8b599b715a2f39928
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-21 23:47:17 -0700

    Question: multiple choice: added multipleChoice question type and its repo. It inherits from Question using joined strategy.

---
+++ src/main/java/wbdv/models/MultipleChoiceQuestion.java
@@ -0,0 +1,25 @@
+package wbdv.models;
+
+import javax.persistence.Entity;
+
+@Entity
+// As we use join strategy for Question, there will be a table for this
+// type too.
+public class MultipleChoiceQuestion extends Question{
+	private String options;
+	private int correctOption;
+	public String getOptions() {
+		return options;
+	}
+	public void setOptions(String options) {
+		this.options = options;
+	}
+	public int getCorrectOption() {
+		return correctOption;
+	}
+	public void setCorrectOption(int correctOption) {
+		this.correctOption = correctOption;
+	}
+	
+	
+}
---
+++ src/main/java/wbdv/repositories/MultipleChoiceQuestionRepository.java
@@ -0,0 +1,8 @@
+package wbdv.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import wbdv.models.MultipleChoiceQuestion;
+
+public interface MultipleChoiceQuestionRepository extends CrudRepository<MultipleChoiceQuestion, Integer>{
+
+}
