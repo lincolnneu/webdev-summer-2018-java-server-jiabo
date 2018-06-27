581a65028f19fb8aef9f62616b99de1ea199943b
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-26 22:53:00 -0700

    Topic: Added relationship between topic and exam and that between topic and assignment.

--- src/main/java/wbdv/models/Topic.java
+++ src/main/java/wbdv/models/Topic.java
@@ -25,6 +25,14 @@ public class Topic {
 	@JsonIgnore
 	private List<Widget> widgets;
 	
+	@OneToMany(mappedBy="topic")
+	@JsonIgnore
+	private List<Exam> exams;
+	
+	@OneToMany(mappedBy="topic")
+	@JsonIgnore
+	private List<Assignment> assignments;
+	
 	public List<Widget> getWidgets() {
 		return widgets;
 	}
@@ -48,5 +56,17 @@ public class Topic {
 	}
 	public void setLesson(Lesson lesson) {
 		this.lesson = lesson;
+	}
+	public List<Exam> getExams() {
+		return exams;
+	}
+	public void setExams(List<Exam> exams) {
+		this.exams = exams;
+	}
+	public List<Assignment> getAssignments() {
+		return assignments;
+	}
+	public void setAssignments(List<Assignment> assignments) {
+		this.assignments = assignments;
 	}
 }
