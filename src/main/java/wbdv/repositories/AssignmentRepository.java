9abfa85934331740f5d85f50f4f48b6ef25c3f47
lincolnneu <hu.jiab@husky.neu.edu>
2018-06-26 22:53:24 -0700

    Assignment: Implemented CRUD repository for assignment.

---
+++ src/main/java/wbdv/repositories/AssignmentRepository.java
@@ -0,0 +1,12 @@
+package wbdv.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import wbdv.models.Assignment;
+
+public interface AssignmentRepository extends CrudRepository<Assignment, Integer>{
+
+}
+
+
+
